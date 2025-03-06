1fb590f971b55caabcfb3bc0ba8a82d85b9a9686
André Perotto <138815928+andreperotto@users.noreply.github.com>
2025-03-06 16:04:23 -0300

    Update StoresPdp.tsx
    price on e off

--- components/ui/StoresPdp.tsx
+++ components/ui/StoresPdp.tsx
@@ -26,6 +26,7 @@ export interface Props {
   whatsNormalButton: WhatsNormalButton;
   showPriceText?: boolean;
   priceText?: string;
+  showPrice?: boolean;
   vehicleDescription?: HTML;
   showVehicleOptional?: boolean;
   vehicleOptionalTitle?: string;
@@ -41,29 +42,24 @@ export default function StoresPdp(
     whatsNormalButton,
     showPriceText,
     priceText,
+    showPrice = true,
     vehicleDescription,
     showVehicleOptional,
     vehicleOptionalTitle,
   }: Props,
 ) {
-
   if (page) {
     console.log(page);
     const vehicle = page.result[0];
     const { storeDataFromApi, idLoja } = page;
     
-    // Inicializa o array de imagens com a imagem principal
     const images:Array<string> = [...vehicle["g:image_link"]];
-    // Checa se há imagens adicionais e as trata adequadamente
     if (vehicle["g:additional_image_link"] && vehicle["g:additional_image_link"][0]) {
       const additionalImagesStr = vehicle["g:additional_image_link"][0];
-      // Verifica se há múltiplas imagens adicionais
       if (additionalImagesStr.includes(", ")) {
-        // Divide as imagens adicionais e as adiciona ao array de imagens
         const additionalImages:Array<string> = additionalImagesStr.split(", ");
         additionalImages.forEach((image: string) => images.push(image));
       } else {
-        // Caso contrário, adiciona a única imagem adicional ao array de imagens
         images.push(additionalImagesStr);
       }
     }
@@ -76,23 +72,17 @@ export default function StoresPdp(
         </Head>
         <div>
           <div class="max-w-[1280px] py-[96px] sm:py-[112px] mx-auto flex flex-col sm:flex-row">
-            <div
-              class={`w-full sm:w-1/2 px-5 pt-5 sm:px-0 flex gap-3 sm:flex-wrap sm:pt-10 overflow-auto scrollbar-none ${
+            <div class="w-full sm:w-1/2 px-5 pt-5 sm:px-0 flex gap-3 sm:flex-wrap sm:pt-10 overflow-auto scrollbar-none ${
                 vehicle["g:image_link"].length == 1 && "items-center"
-              }`}
-            >
-              <GalleryProductPage
-                images={images}
-              />
+              }">
+              <GalleryProductPage images={images} />
               <div class="hidden sm:flex sm:flex-col">
                 <VehicleOptional
                   vehicle={vehicle}
                   showVehicleOptional={showVehicleOptional}
                   vehicleOptionalTitle={vehicleOptionalTitle}
-                  />
-                <VehicleDescription
-                  description={vehicleDescription}
                 />
+                <VehicleDescription description={vehicleDescription} />
               </div>
             </div>
 
@@ -103,6 +93,7 @@ export default function StoresPdp(
                 whatsNormalButton={whatsNormalButton}
                 showPriceText={showPriceText}
                 priceText={priceText}
+                showPrice={showPrice}
               />
 
               <div class="flex flex-col mt-5 sm:hidden">
@@ -112,9 +103,7 @@ export default function StoresPdp(
                   vehicleOptionalTitle={vehicleOptionalTitle}
                 />
                 
-                <VehicleDescription
-                  description={vehicleDescription}
-                />
+                <VehicleDescription description={vehicleDescription} />
               </div>
             </div>
           </div>
@@ -129,31 +118,17 @@ export default function StoresPdp(
 
 function GalleryProductPage({ images }: { images: string[] }) {
   const id = useId();
-
   return (
-    <div
-      id={id}
-      class="grid grid-cols-[48px_1fr_48px] sm:grid-cols-[120px_1fr_120px] grid-rows-[1fr_48px_1fr_64px]"
-    >
+    <div id={id} class="grid grid-cols-[48px_1fr_48px] sm:grid-cols-[120px_1fr_120px] grid-rows-[1fr_48px_1fr_64px]">
       <Slider class="carousel carousel-center w-full col-span-full row-span-full gap-2">
         {images?.map((image, index) => (
-          <Slider.Item
-            index={index}
-            class="carousel-item w-full flex items-center"
-          >
-            <Image
-              class="w-full h-max"
-              src={image}
-              width={550}
-            />
+          <Slider.Item index={index} class="carousel-item w-full flex items-center">
+            <Image class="w-full h-max" src={image} width={550} />
           </Slider.Item>
         ))}
       </Slider>
-
       {images?.length! > 1 ? <Buttons /> : null}
-
       <Dots images={images} />
-
       <SliderJS rootId={id} infinite />
     </div>
   );
@@ -167,11 +142,7 @@ function Dots({ images }: { images: string[] }) {
           <li class="carousel-item">
             <Slider.Dot index={index}>
               <div class="w-14 h-12 py-5 border-[1px] border-black flex justify-center items-center opacity-40 group-disabled:opacity-100">
-                <Image
-                  class="w-[40px] h-max"
-                  src={image}
-                  width={80}
-                />
+                <Image class="w-[40px] h-max" src={image} width={80} />
               </div>
             </Slider.Dot>
           </li>
@@ -186,22 +157,12 @@ function Buttons() {
     <>
       <div class="flex items-center justify-center z-10 col-start-1 row-start-2">
         <Slider.PrevButton>
-          <Icon
-            class="text-white"
-            size={26}
-            id="ChevronLeft"
-            strokeWidth={5}
-          />
+          <Icon class="text-white" size={26} id="ChevronLeft" strokeWidth={5} />
         </Slider.PrevButton>
       </div>
       <div class="flex items-center justify-center z-10 col-start-3 row-start-2">
         <Slider.NextButton>
-          <Icon
-            class="text-white"
-            size={26}
-            id="ChevronRight"
-            strokeWidth={5}
-          />
+          <Icon class="text-white" size={26} id="ChevronRight" strokeWidth={5} />
         </Slider.NextButton>
       </div>
     </>
